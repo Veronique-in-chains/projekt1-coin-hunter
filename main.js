2363ca7ede010af767ac2f01dcb02295fd64ebd1
Veronika Bosáková <veronikabosakova@gmail.com>
2022-02-27 22:46:54 +0100

    rozhýbání panáčka

--- main.js
+++ main.js
@@ -7,3 +7,26 @@ if (!( panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || pan
 
 
 // sem začni psát svůj program
+
+
+function stiskKlavesy(event) {
+  let sipka = event.key;
+  let panacek = document.getElementById("panacek");
+  let currentTop = parseInt(window.getComputedStyle(panacek).getPropertyValue("top"));
+  let currentLeft = parseInt(window.getComputedStyle(panacek).getPropertyValue("left"));
+  let step = 10;
+  if (sipka === "ArrowDown") {
+    panacek.style.top = (currentTop + step) + "px";
+    panacek.src = "obrazky/panacek.png";
+  } else if (sipka === "ArrowRight") {
+    panacek.style.left = (currentLeft + step) + "px";
+    panacek.src = "obrazky/panacek-vpravo.png";
+  } else if (sipka === "ArrowLeft") {
+    panacek.style.left = (currentLeft - step) + "px";
+    panacek.src = "obrazky/panacek-vlevo.png";
+  } else if (sipka === "ArrowUp") {
+    panacek.style.top = (currentTop - step) + "px";
+    panacek.src = "obrazky/panacek-nahoru.png";
+
+  }
+}
