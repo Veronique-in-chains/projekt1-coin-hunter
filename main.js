739abf6a103344238a368180b7dc70086ca235f6
Veronika Bosáková <veronikabosakova@gmail.com>
2022-03-12 12:21:07 +0100

    hotový projekt

--- main.js
+++ main.js
@@ -7,27 +7,50 @@ if (!( panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || pan
 
 
 // sem začni psát svůj program
+let panacek, panacekSirka, panacekVyska, currentTop, currentLeft;
+let mince, minceLeft, minceTop, minceY, minceX, minceSirka, minceVyska;
+let step, score;
+
 
 //náhodné umístění mince
-let mince = document.getElementById("mince");
-
+mince = document.getElementById("mince");
 function umisteniMince() {
-  let width = window.innerHeight;
-  let height = window.innerWidth;
-  let minceTop = Math.random() * width - 30;
-  let minceLeft = Math.random() * height - 30;
+  minceTop = Math.random() * window.innerHeight - 35;
+  minceLeft = Math.random() * window.innerWidth - 35;
   mince.style.left = minceLeft + "px";
   mince.style.top = minceTop + "px";
+  mince.src = "obrazky/mince-" + Math.ceil(Math.random() * 3) + ".png";
 }
 umisteniMince();
 
+
+panacek = document.getElementById("panacek");
+function umisteniPanacka() {
+  panacek.style.top = (Math.ceil(Math.random() * window.innerHeight)) + "px";
+  panacek.style.left = (Math.ceil(Math.random() * window.innerWidth)) + "px";
+}
+umisteniPanacka();
+
+//spuštění hudby
+function spustHudbu() {
+  let audio = document.getElementById("hudba");
+  audio.play();
+}
+
 //pohyb panáčka
-let panacek = document.getElementById("panacek");
+
+
+score = 0;
+
+
+alert("Vítej ve hře! Pomoz panáčkovi posbírat pět mincí! Ovládat jej můžeš šipkami na klávesnici.")
+
 
 function stiskKlavesy(event) {
-  let currentTop = parseInt(window.getComputedStyle(panacek).getPropertyValue("top"));
-  let currentLeft = parseInt(window.getComputedStyle(panacek).getPropertyValue("left"));
-  let step = 5;
+  //spustHudbu();
+  currentTop = parseInt(window.getComputedStyle(panacek).getPropertyValue("top"));
+  currentLeft = parseInt(window.getComputedStyle(panacek).getPropertyValue("left"));
+  step = 10;
   switch(event.key) {
     case "ArrowDown": 
       panacek.style.top = (currentTop + step) + "px";
@@ -64,25 +87,25 @@ function stiskKlavesy(event) {
 
 
   // sebrání mince
-  let minceY = parseInt(window.getComputedStyle(mince).getPropertyValue("top"));
-  let minceX = parseInt(window.getComputedStyle(mince).getPropertyValue("left"));
-  let panacekSirka = parseInt(window.getComputedStyle(panacek).getPropertyValue("width"));
-  let panacekVyska = parseInt(window.getComputedStyle(panacek).getPropertyValue("height"));
-  let minceSirka = parseInt(window.getComputedStyle(mince).getPropertyValue("width"));
-  let minceVyska = parseInt(window.getComputedStyle(mince).getPropertyValue("height"));
-
+  minceY = parseInt(window.getComputedStyle(mince).getPropertyValue("top"));
+  minceX = parseInt(window.getComputedStyle(mince).getPropertyValue("left"));
+  panacekSirka = panacek.width;
+  panacekVyska = panacek.height;
+  minceSirka = mince.width;
+  minceVyska = mince.height;
+  
   if (!(currentLeft + panacekSirka < minceX || minceX + minceSirka < currentLeft || currentTop + panacekVyska < minceY || minceY + minceVyska < currentTop)) {
-      let score = 0;
+      let zvukmince = document.getElementById("zvukmince");
+      zvukmince.play();
       score++;
       document.getElementById("score").innerHTML = score;
       console.log(score);
+      if (score === 5) {
+        let zvukfanfara = document.getElementById("zvukfanfara");
+        zvukfanfara.play();
+        alert("Sebral jsi pět mincí. Vyhrál jsi! Můžeš ale sbírat dál...");
+      }
       umisteniMince();
   }
 }
 
-//spuštění hudby
-function spustHudbu() {
-  let audio = document.getElementById("hudba");
-  audio.play();
-}
-window.addEventListener(keydown, spustHudbu());
